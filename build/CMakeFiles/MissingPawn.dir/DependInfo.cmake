88c9099f3af41c27d0b1bb7fc9a51437d6de1715
MissigPlayer <k.u.hacker123@gmail.com>
2026-07-24 11:34:25 +0200

    Add files via upload

---
+++ build/CMakeFiles/MissingPawn.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Engine/HistoryHeuristic.cpp" "CMakeFiles/MissingPawn.dir/Engine/HistoryHeuristic.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Engine/HistoryHeuristic.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Engine/KillerMoves.cpp" "CMakeFiles/MissingPawn.dir/Engine/KillerMoves.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Engine/KillerMoves.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Engine/MoveOrdering.cpp" "CMakeFiles/MissingPawn.dir/Engine/MoveOrdering.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Engine/MoveOrdering.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Engine/Search.cpp" "CMakeFiles/MissingPawn.dir/Engine/Search.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Engine/Search.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Engine/Transposition.cpp" "CMakeFiles/MissingPawn.dir/Engine/Transposition.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Engine/Transposition.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Foundation/Board.cpp" "CMakeFiles/MissingPawn.dir/Foundation/Board.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Foundation/Board.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Game/Fen.cpp" "CMakeFiles/MissingPawn.dir/Game/Fen.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Game/Fen.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Game/GameHistory.cpp" "CMakeFiles/MissingPawn.dir/Game/GameHistory.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Game/GameHistory.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Game/GameState.cpp" "CMakeFiles/MissingPawn.dir/Game/GameState.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Game/GameState.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Game/Zobrist.cpp" "CMakeFiles/MissingPawn.dir/Game/Zobrist.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Game/Zobrist.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Move/MoveGenerator.cpp" "CMakeFiles/MissingPawn.dir/Move/MoveGenerator.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Move/MoveGenerator.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Move/MoveParser.cpp" "CMakeFiles/MissingPawn.dir/Move/MoveParser.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Move/MoveParser.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Move/MoveValidator.cpp" "CMakeFiles/MissingPawn.dir/Move/MoveValidator.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Move/MoveValidator.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Others/TimeManager.cpp" "CMakeFiles/MissingPawn.dir/Others/TimeManager.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Others/TimeManager.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Others/UCI/UCI.cpp" "CMakeFiles/MissingPawn.dir/Others/UCI/UCI.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Others/UCI/UCI.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Others/UCI/UCIInfoPrinter.cpp" "CMakeFiles/MissingPawn.dir/Others/UCI/UCIInfoPrinter.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Others/UCI/UCIInfoPrinter.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Others/UCI/UCIOptionParser.cpp" "CMakeFiles/MissingPawn.dir/Others/UCI/UCIOptionParser.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Others/UCI/UCIOptionParser.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Others/UCI/UCIOptions.cpp" "CMakeFiles/MissingPawn.dir/Others/UCI/UCIOptions.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Others/UCI/UCIOptions.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Others/UCI/UCIParser.cpp" "CMakeFiles/MissingPawn.dir/Others/UCI/UCIParser.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Others/UCI/UCIParser.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Others/UCI/UCIPositionParser.cpp" "CMakeFiles/MissingPawn.dir/Others/UCI/UCIPositionParser.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Others/UCI/UCIPositionParser.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Rate/Bishop.cpp" "CMakeFiles/MissingPawn.dir/Rate/Bishop.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Rate/Bishop.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Rate/Evaluation.cpp" "CMakeFiles/MissingPawn.dir/Rate/Evaluation.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Rate/Evaluation.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Rate/King.cpp" "CMakeFiles/MissingPawn.dir/Rate/King.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Rate/King.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Rate/Knight.cpp" "CMakeFiles/MissingPawn.dir/Rate/Knight.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Rate/Knight.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Rate/Pawn.cpp" "CMakeFiles/MissingPawn.dir/Rate/Pawn.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Rate/Pawn.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Rate/Queen.cpp" "CMakeFiles/MissingPawn.dir/Rate/Queen.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Rate/Queen.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/Rate/Rook.cpp" "CMakeFiles/MissingPawn.dir/Rate/Rook.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/Rate/Rook.cpp.obj.d"
+  "C:/Users/kuhac/OneDrive/Dokumenty/Missing Pawn/Source Code/main.cpp" "CMakeFiles/MissingPawn.dir/main.cpp.obj" "gcc" "CMakeFiles/MissingPawn.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MissingPawn.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MissingPawn.
+# This may be replaced when dependencies are built.
